fd2b5fdb58b774a96e9c1639193a7da175d27d9c
ayushvaidya <ayush.everest@gmail.com>
2019-02-10 03:57:04 -0500

    ok

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,9 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = current_user.projects.all
+    if user_signed_in?
+      @projects = current_user.projects
+    end
   end
 
   # GET /projects/1
